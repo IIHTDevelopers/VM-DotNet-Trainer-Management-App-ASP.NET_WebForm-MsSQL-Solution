52339ae56a78ed55b26d423e111d9828499cc593
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-11 11:23:12 +0530

    Add files via upload

---
+++ TrainerManagementApp/DAL/Services/TrainerRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using TrainerManagementApp.DAL.Interfaces;
+
+namespace TrainerManagementApp.DAL.Services
+{
+    public class TrainerRepository : Interfaces.ITrainerRepository
+    {
+        private TrainerDbContext _context;
+
+        public TrainerRepository(TrainerDbContext context)
+        {
+            _context = context;
+        }
+
+        public Model.TrainerModel GetById(string id)
+        {
+            return _context.TrainerModels.FirstOrDefault(t => t.Id == int.Parse(id));
+        }
+
+        public string GetAll()
+        {
+            string qry = "select* from TrainerModels";
+            return qry;
+        }
+
+        public string Add()
+        {
+            string qry = "insert into TrainerModels(FirstName, LastName, DateOfBirth)" +
+                "values('";
+            return qry;
+        }
+
+        public string Update()
+        {
+            var query = "update TrainerModels set FirstName='";
+            return query;
+        }
+
+        public string Delete()
+        {
+            var query = "delete from TrainerModels where Id='";
+            return query;
+        }
+    }
+}
